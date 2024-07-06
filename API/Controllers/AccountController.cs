0368e74f5e4a1e522268f732f8800f26356ea841
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-06 16:55:04 +0330

    feat: Add 'AccountController' to Manage 'User Accounts' and 'Authentication'

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,106 @@
+﻿using Core.Domain.IdentityEntities;
+using Core.DTO.Auth;
+using Core.ServiceContracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IJwtService _jwtService;
+    
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IJwtService jwtService)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _jwtService = jwtService;
+    }
+
+
+    // Register//
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserRegister registerDTO)
+    {
+        // Validation
+        if (ModelState.IsValid == false)
+        {
+            string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+            return Problem(errorMessage);
+        }
+        
+        ApplicationUser user = new ApplicationUser()
+        {
+            UserName = registerDTO.Email,
+            Email = registerDTO.Email,
+            PhoneNumber = registerDTO.Phone
+        };
+
+        IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
+        if (result.Succeeded)
+        {
+            // Sign in
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            
+            var authenticationResponse = _jwtService.CreateJwtToken(user);
+
+            return Ok(authenticationResponse);
+        }
+        else
+        {
+            string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description)); //error1 | error2
+            return Problem(errorMessage);
+        }   
+    }
+    
+        
+    // Login //
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserLogin loginDTO)
+    {
+        // Validation
+        if (ModelState.IsValid == false)
+        {
+            string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+            return Problem(errorMessage);
+        }
+        
+        var result = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Password, isPersistent: false, lockoutOnFailure: false);
+
+        if (result.Succeeded)
+        {
+            ApplicationUser? user = await _userManager.FindByEmailAsync(loginDTO.Email);
+
+            if (user == null)
+            {
+                return NoContent();
+            }
+
+            // sign-in
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            var authenticationResponse = _jwtService.CreateJwtToken(user);
+
+            return Ok(authenticationResponse);
+        }
+
+        else
+        {
+            return Problem("Invalid email or password");
+        }
+    }
+        
+        
+    // Logout //
+    [HttpGet("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+
+        return NoContent();
+    }
+}

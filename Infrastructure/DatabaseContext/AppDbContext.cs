34f08f54557808db4788d0034f89eb38305c9a51
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-07 17:12:47 +0330

    feat: Add Some DbSet's to 'AppDbContext'

--- Infrastructure/DatabaseContext/AppDbContext.cs
+++ Infrastructure/DatabaseContext/AppDbContext.cs
@@ -1,11 +1,17 @@
-﻿using Core.Domain.IdentityEntities;
+﻿using Core.Domain.Entities;
+using Core.Domain.IdentityEntities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Infrastructure.DatabaseContext;
 
 public class AppDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,Guid>
 {
+    public virtual DbSet<UserProfile> UserProfiles { get; set; }
+    public virtual DbSet<DefinedAccount> DefinedAccounts { get; set; }
+    public virtual DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+    public virtual DbSet<Currency> Currencies { get; set; }
     
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
@@ -15,5 +21,11 @@ public class AppDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,Gu
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Currency>()
+            .Property(entity => entity.ExchangeRates)
+            .HasConversion(
+                v => JsonConvert.SerializeObject(v),
+                v => JsonConvert.DeserializeObject<Dictionary<int, decimal>>(v));
     }
 }

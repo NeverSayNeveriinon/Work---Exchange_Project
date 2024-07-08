be66aeb7b4e6b34f84929c61b39b6ec81e8b95be
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-08 17:02:50 +0330

    feat: Create 'CurrencyAccountRepository' and its Interface

---
+++ Core/Domain/RepositoryContracts/ICurrencyAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Domain.Entities;
+
+namespace Core.Domain.RepositoryContracts;
+
+public interface ICurrencyAccountRepository
+{
+    public Task<List<CurrencyAccount>> GetAllCurrencyAccounts();
+    public Task<CurrencyAccount?> GetCurrencyAccountByID(int id);
+    public Task<CurrencyAccount> AddCurrencyAccount(CurrencyAccount account);
+    public Task<CurrencyAccount> UpdateCurrencyAccount(CurrencyAccount account, CurrencyAccount updatedCurrencyAccount);
+    public Task<bool> DeleteCurrencyAccount(CurrencyAccount account);
+    public Task SaveChangesAsync();
+}
---
+++ Infrastructure/Repositories/CurrencyAccountRepository.cs
@@ -0,0 +1,65 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.RepositoryContracts;
+using Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CurrencyAccountRepository : ICurrencyAccountRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    
+    public CurrencyAccountRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+   
+
+    public async Task<List<CurrencyAccount>> GetAllCurrencyAccounts()
+    {
+        var accounts = _dbContext.CurrencyAccounts.AsNoTracking();
+        
+        List<CurrencyAccount> accountsList = await accounts.ToListAsync();
+        
+        return accountsList;
+    }
+
+    public async Task<CurrencyAccount?> GetCurrencyAccountByID(int id)
+    {
+        CurrencyAccount? account = await _dbContext.CurrencyAccounts
+                                              .AsNoTracking()
+                                              .FirstOrDefaultAsync(accountItem => accountItem.Number == id);
+
+        return account;
+    }
+
+     
+    public async Task<CurrencyAccount> AddCurrencyAccount(CurrencyAccount account)
+    {
+        await _dbContext.CurrencyAccounts.AddAsync(account);
+
+        return account;
+    }
+    
+    public async Task<CurrencyAccount> UpdateCurrencyAccount(CurrencyAccount account, CurrencyAccount updatedCurrencyAccount)
+    {
+        _dbContext.Entry(account).State = EntityState.Modified;
+
+        _dbContext.Entry(account).CurrentValues.SetValues(updatedCurrencyAccount);
+        
+        return account;
+    }
+    
+    public async Task<bool> DeleteCurrencyAccount(CurrencyAccount account)
+    {
+        var entityEntry = _dbContext.CurrencyAccounts.Remove(account);
+        
+        return entityEntry.State == EntityState.Deleted;
+    }
+    
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}

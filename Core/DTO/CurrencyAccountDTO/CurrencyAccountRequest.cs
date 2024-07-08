efafacc913cc655198fd8bceb886b417cd4f0f35
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-08 17:08:03 +0330

    feat: Create 'CurrencyAccountRequest' and 'CurrencyAccountResponse' DTO's

---
+++ Core/DTO/CurrencyAccountDTO/CurrencyAccountRequest.cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.Entities;
+using Core.Enums;
+
+namespace Core.DTO.CurrencyAccountDTO;
+
+public class CurrencyAccountRequest
+{
+    public string CuurrencyType { get; set; }
+}
+
+public static partial class CurrencyAccountExtensions
+{
+    public static CurrencyAccount ToCurrencyAccount(this CurrencyAccountRequest currencyAccountRequest)
+    {
+        CurrencyAccount currencyAccount = new CurrencyAccount()
+        {
+            Number = 0,
+            Balance = 0,
+            Currency = new Currency(){CurrencyType = (CurrencyTypeOptions)Enum.Parse(typeof(CurrencyTypeOptions), currencyAccountRequest.CuurrencyType)}
+        };
+
+        return currencyAccount;
+    }
+}
---
+++ Core/DTO/CurrencyAccountDTO/CurrencyAccountResponse.cs
@@ -0,0 +1,31 @@
+﻿using Core.Domain.Entities;
+
+namespace Core.DTO.CurrencyAccountDTO;
+
+public class CurrencyAccountResponse
+{
+    public int Number { get; set; }
+    public Guid OwnerId { get; set; }
+    public string FullName { get; set; }
+    public decimal Balance { get; set; }
+    public string CurrencyType { get; set; }
+
+}
+
+
+public static partial class CurrencyAccountExtensions
+{
+    public static CurrencyAccountResponse ToCurrencyAccountResponse (this CurrencyAccount currencyAccount)
+    {
+        CurrencyAccountResponse response = new CurrencyAccountResponse()
+        {
+            Number = currencyAccount.Number,
+            OwnerId = currencyAccount.OwnerID,
+            FullName = currencyAccount.Owner.Name,
+            Balance = currencyAccount.Balance,
+            CurrencyType = nameof(currencyAccount.Currency.CurrencyType)
+        };
+
+        return response;
+    }
+}

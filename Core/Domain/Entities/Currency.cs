212c2d47bbecb654cedd16de68eb3db0b79d889e
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-09 17:20:13 +0330

    feat: Add a New Navigation to 'Currency' Entity

--- Core/Domain/Entities/Currency.cs
+++ Core/Domain/Entities/Currency.cs
@@ -13,13 +13,19 @@ public class Currency
     // Relations //
     #region Relations
     
-    // With "Currency(As SecondCurrency)" ---> FirstCurrency 'N'====......===='N' SecondCurrency -> in 'ExchangeRate' Entity
-    public List<Currency>? SecondCurrencies { get; } = new List<Currency>(); // Navigation to 'Currency(As SecondCurrency)' entity
-    public List<ExchangeRate>? FirstExchangeRates { get; } = new List<ExchangeRate>(); // Navigation to 'ExchangeRate(Join Entity)' entity
     
-    // With "Currency(As FirstCurrency)" ---> SecondCurrency 'N'====......===='N' FirstCurrency -> in 'ExchangeRate' Entity
+    //                              (Dependent)                        (Principal)
+    // With "CurrencyAccount" ---> CurrencyAccount 'N'====......----'1' Currency
+    public ICollection<CurrencyAccount>? CurrencyAccounts { get; } = new List<CurrencyAccount>(); // Navigation to 'CurrencyAccount' entity
+    
+    
+    // With "Currency(As SecondCurrency)" ---> FirstCurrency 'N'====......===='N' SecondCurrency -> in 'ExchangeValue' Entity
+    public List<Currency>? SecondCurrencies { get; } = new List<Currency>(); // Navigation to 'Currency(As SecondCurrency)' entity
+    public List<ExchangeValue>? FirstExchangeValues { get; } = new List<ExchangeValue>(); // Navigation to 'ExchangeValue(Join Entity)' entity
+    
+    // With "Currency(As FirstCurrency)" ---> SecondCurrency 'N'====......===='N' FirstCurrency -> in 'ExchangeValue' Entity
     public List<Currency>? FirstCurrencies { get; } = new List<Currency>(); // Navigation to 'Currency(As FirstCurrency)' entity
-    public List<ExchangeRate>? SecondExchangeRates { get; } = new List<ExchangeRate>(); // Navigation to 'ExchangeRate(Join Entity)' entity
+    public List<ExchangeValue>? SecondExchangeValues { get; } = new List<ExchangeValue>(); // Navigation to 'ExchangeValue(Join Entity)' entity
     
     #endregion
 }

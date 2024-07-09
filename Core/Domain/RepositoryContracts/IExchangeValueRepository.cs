b7f0361aa0a400de44f36810e5fdac7e715d4650
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-09 17:26:39 +0330

    feat: Create 'ExchangeValueRepository' and its Interface

---
+++ Core/Domain/RepositoryContracts/IExchangeValueRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Domain.Entities;
+
+namespace Core.Domain.RepositoryContracts;
+
+public interface IExchangeValueRepository
+{
+    public Task<List<ExchangeValue>> GetAllExchangeValues();
+    public Task<ExchangeValue?> GetExchangeValueByID(int id);
+    public Task<ExchangeValue> AddExchangeValue(ExchangeValue currency);
+    public Task<ExchangeValue> UpdateExchangeValue(ExchangeValue currency, ExchangeValue updatedExchangeValue);
+    public Task<bool> DeleteExchangeValue(ExchangeValue currency);
+    public Task SaveChangesAsync();
+}
---
+++ Infrastructure/Repositories/ExchangeValueRepository.cs
@@ -0,0 +1,64 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.RepositoryContracts;
+using Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class ExchangeValueRepository : IExchangeValueRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    
+    public ExchangeValueRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+   
+
+    public async Task<List<ExchangeValue>> GetAllExchangeValues()
+    {
+        var currencies = _dbContext.ExchangeValues.AsNoTracking();
+        
+        List<ExchangeValue> currenciesList = await currencies.ToListAsync();
+        
+        return currenciesList;
+    }
+
+    public async Task<ExchangeValue?> GetExchangeValueByID(int id)
+    {
+        ExchangeValue? exchangeValue = await _dbContext.ExchangeValues
+                                             .AsNoTracking()
+                                             .FirstOrDefaultAsync(exchangeValueItem => exchangeValueItem.Id == id);
+
+        return exchangeValue;
+    }
+
+     
+    public async Task<ExchangeValue> AddExchangeValue(ExchangeValue exchangeValue)
+    {
+        var exchangeValueReturned = await _dbContext.ExchangeValues.AddAsync(exchangeValue);
+
+        return exchangeValueReturned.Entity;
+    }
+    
+    public async Task<ExchangeValue> UpdateExchangeValue(ExchangeValue exchangeValue, ExchangeValue updatedExchangeValue)
+    {
+        // _dbContext.Entry(exchangeValue).Property(p => p.ExchangeValueType).IsModified = true;
+        // exchangeValue.ExchangeValueType = updatedExchangeValue.ExchangeValueType;
+
+        return exchangeValue;
+    }
+    
+    public async Task<bool> DeleteExchangeValue(ExchangeValue exchangeValue)
+    {
+        var entityEntry = _dbContext.ExchangeValues.Remove(exchangeValue);
+        
+        return entityEntry.State == EntityState.Deleted;
+    }
+    
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}

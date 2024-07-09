bb12f9fdc2807a536aa434857c1fcd7c072fe9e8
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-09 17:21:57 +0330

    feat: Create Two 'Request and Response' DTO's For 'Currency' Entity

---
+++ Core/DTO/CurrencyDTO/CurrencyRequest.cs
@@ -0,0 +1,23 @@
+﻿using Core.Domain.Entities;
+using Core.Enums;
+
+namespace Core.DTO.CurrencyDTO;
+
+public class CurrencyRequest
+{
+    public string CuurrencyType { get; set; }
+}
+
+public static partial class CurrencyExtensions
+{
+    public static Currency ToCurrency(this CurrencyRequest currencyRequest)
+    {
+        Currency currency = new Currency()
+        {
+            Id = 0,
+            CurrencyType = (CurrencyTypeOptions)Enum.Parse(typeof(CurrencyTypeOptions), currencyRequest.CuurrencyType)
+        };
+
+        return currency;
+    }
+}
---
+++ Core/DTO/CurrencyDTO/CurrencyResponse.cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.Entities;
+using Core.Enums;
+
+namespace Core.DTO.CurrencyDTO;
+
+public class CurrencyResponse
+{
+    public int Id { get; set; }
+    public string CurrencyType  { get; set; }
+}
+
+public static partial class CurrencyExtensions
+{
+    public static CurrencyResponse ToCurrencyResponse (this Currency currency)
+    {
+        CurrencyResponse response = new CurrencyResponse()
+        {
+            Id = currency.Id,
+            CurrencyType = Enum.GetName(typeof (CurrencyTypeOptions), currency.CurrencyType)
+        };
+
+        return response;
+    }
+}

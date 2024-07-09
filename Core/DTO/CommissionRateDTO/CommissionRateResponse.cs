20280dac510989d755bbb2a0b29c94b6c77431ac
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-09 17:25:13 +0330

    feat: Create One 'Response' DTO For 'CommissionRate' Entity

---
+++ Core/DTO/CommissionRateDTO/CommissionRateResponse.cs
@@ -0,0 +1,25 @@
+﻿using Core.Domain.Entities;
+
+namespace Core.DTO.CommissionRateDTO;
+
+public class CommissionRateResponse
+{
+    public int Id { get; set; }
+    public decimal MaxUSDRange { get; set; }
+    public double CRate { get; set; }
+}
+
+public static partial class CommissionRateExtensions
+{
+    public static CommissionRateResponse ToCommissionRateResponse (this CommissionRate commissionRate)
+    {
+        CommissionRateResponse response = new CommissionRateResponse()
+        {
+            Id = commissionRate.Id,
+            MaxUSDRange = commissionRate.MaxUSDRange,
+            CRate = commissionRate.CRate
+        };
+
+        return response;
+    }
+}

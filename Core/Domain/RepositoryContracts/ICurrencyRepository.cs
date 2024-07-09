e2e0f34a4f6b86980d55e88d463eb8f164b5d770
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-09 17:17:50 +0330

    feat: Create 'CurrencyRepository' and its Interface

---
+++ Core/Domain/RepositoryContracts/ICurrencyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Domain.Entities;
+
+namespace Core.Domain.RepositoryContracts;
+
+public interface ICurrencyRepository
+{
+    public Task<List<Currency>> GetAllCurrencies();
+    public Task<Currency?> GetCurrencyByID(int id);
+    public Task<Currency> AddCurrency(Currency currency);
+    public Task<Currency> UpdateCurrency(Currency currency, Currency updatedCurrency);
+    public Task<bool> DeleteCurrency(Currency currency);
+    public Task SaveChangesAsync();
+}
---
+++ Infrastructure/Repositories/CurrencyRepository.cs
@@ -0,0 +1,64 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.RepositoryContracts;
+using Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CurrencyRepository : ICurrencyRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    
+    public CurrencyRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+   
+
+    public async Task<List<Currency>> GetAllCurrencies()
+    {
+        var currencies = _dbContext.Currencies.AsNoTracking();
+        
+        List<Currency> currenciesList = await currencies.ToListAsync();
+        
+        return currenciesList;
+    }
+
+    public async Task<Currency?> GetCurrencyByID(int id)
+    {
+        Currency? currency = await _dbContext.Currencies
+                                             .AsNoTracking()
+                                             .FirstOrDefaultAsync(currencyItem => currencyItem.Id == id);
+
+        return currency;
+    }
+
+     
+    public async Task<Currency> AddCurrency(Currency currency)
+    {
+        var currencyReturned = await _dbContext.Currencies.AddAsync(currency);
+
+        return currencyReturned.Entity;
+    }
+    
+    public async Task<Currency> UpdateCurrency(Currency currency, Currency updatedCurrency)
+    {
+        _dbContext.Entry(currency).Property(p => p.CurrencyType).IsModified = true;
+        currency.CurrencyType = updatedCurrency.CurrencyType;
+
+        return currency;
+    }
+    
+    public async Task<bool> DeleteCurrency(Currency currency)
+    {
+        var entityEntry = _dbContext.Currencies.Remove(currency);
+        
+        return entityEntry.State == EntityState.Deleted;
+    }
+    
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}

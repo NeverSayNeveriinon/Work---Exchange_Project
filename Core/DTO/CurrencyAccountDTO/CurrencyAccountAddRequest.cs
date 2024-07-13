364c96d0811262689341dff0198c5080ab3d8c42
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-13 09:35:40 +0330

    refactor: Remove OwnerID from 'CurrencyAccountAddRequest'

--- Core/DTO/CurrencyAccountDTO/CurrencyAccountAddRequest.cs
+++ Core/DTO/CurrencyAccountDTO/CurrencyAccountAddRequest.cs
@@ -7,12 +7,11 @@ namespace Core.DTO.CurrencyAccountDTO;
 public class CurrencyAccountAddRequest
 {
     public int CurrencyId { get; set; }
-    public Guid OwnerID { get; set; }
 }
 
 public static partial class CurrencyAccountExtensions
 {
-    public static CurrencyAccount ToCurrencyAccount(this CurrencyAccountAddRequest currencyAccountAddRequest)
+    public static CurrencyAccount ToCurrencyAccount(this CurrencyAccountAddRequest currencyAccountAddRequest, Guid? OwnerID)
     {
         CurrencyAccount currencyAccount = new CurrencyAccount()
         {
@@ -20,7 +19,7 @@ public static partial class CurrencyAccountExtensions
             Balance = 0,
             // Currency = new Currency(){CurrencyType = (CurrencyTypeOptions)Enum.Parse(typeof(CurrencyTypeOptions), CurrencyAccountAddRequest.CurrencyType)},
             CurrencyID = currencyAccountAddRequest.CurrencyId,
-            OwnerID = currencyAccountAddRequest.OwnerID
+            OwnerID = OwnerID.Value
             // Currency = new Currency(){Id = CurrencyAccountAddRequest.CurrencyId},
             // Owner = new UserProfile(){Id = CurrencyAccountAddRequest.OwnerID}
         };

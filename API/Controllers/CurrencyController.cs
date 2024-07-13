e81be81f364e4a69f43713321c8b8c34eba68015
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-13 09:51:29 +0330

    feat: Add 'Role Based Authorization' with 'JWT'

--- API/Controllers/CurrencyController.cs
+++ API/Controllers/CurrencyController.cs
@@ -1,11 +1,14 @@
 ﻿using Core.DTO.CurrencyDTO;
 using Core.ServiceContracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [Route("api/[controller]")] 
 [ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
 public class CurrencyController : ControllerBase
 {
     private readonly ICurrencyService _currencyService;

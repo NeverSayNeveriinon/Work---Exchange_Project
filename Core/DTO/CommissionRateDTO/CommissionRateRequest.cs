37f325ac0fdaa3ad6484f4737d34342b2353d5f9
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-13 09:28:26 +0330

    feat: Add 'CommissionRequest' and Change code to use that

---
+++ Core/DTO/CommissionRateDTO/CommissionRateRequest.cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.Entities;
+
+namespace Core.DTO.CommissionRateDTO;
+
+public class CommissionRateRequest
+{
+    public decimal MaxUSDRange { get; set; }
+    public double CRate { get; set; }
+}
+
+public static partial class CommissionRateExtensions
+{
+    public static CommissionRate ToCommissionRate (this CommissionRateRequest commissionRate)
+    {
+        CommissionRate response = new CommissionRate()
+        {
+            Id = 0,
+            MaxUSDRange = commissionRate.MaxUSDRange,
+            CRate = commissionRate.CRate
+        };
+
+        return response;
+    }
+}

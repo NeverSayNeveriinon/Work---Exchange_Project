58f91d42bf4ec70c667ed29393d23d2feedf1544
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-14 16:27:23 +0330

    feat&refactor: Delete old 'TransactionAddRequest' and Add Two Type of 'TransactionRequest' DTO

---
+++ Core/DTO/TransactionDTO/TransactionDepositAddRequest.cs
@@ -0,0 +1,29 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.IdentityEntities;
+using Core.Enums;
+
+namespace Core.DTO.TransactionDTO;
+
+public class TransactionDepositAddRequest
+{
+    public MoneyRequest money { get; set; }
+    public int AccountNumber { get; set; }
+}
+
+public static partial class TransactionExtensions
+{
+    public static Transaction ToTransaction(this TransactionDepositAddRequest transactionAddRequest)
+    {
+        Transaction transaction = new Transaction()
+        {
+            Id = 0,
+            Amount = 0,
+            FromAccountNumber = transactionAddRequest.AccountNumber,
+            ToAccountNumber = null,
+            DateTime = DateTime.Now,
+            TransactionType = TransactionTypeOptions.Deposit
+        };
+
+        return transaction;
+    }
+}
--- Core/DTO/TransactionDTO/TransactionTransferAddRequest.cs
+++ Core/DTO/TransactionDTO/TransactionTransferAddRequest.cs
@@ -4,16 +4,16 @@ using Core.Enums;
 
 namespace Core.DTO.TransactionDTO;
 
-public class TransactionAddRequest
+public class TransactionTransferAddRequest
 {
     public decimal Amount { get; set; }
     public int FromAccountNumber { get; set; }
-    public int ToAccountNumber { get; set; }
+    public int? ToAccountNumber { get; set; }
 }
 
 public static partial class TransactionExtensions
 {
-    public static Transaction ToTransaction(this TransactionAddRequest transactionAddRequest)
+    public static Transaction ToTransaction(this TransactionTransferAddRequest transactionAddRequest)
     {
         Transaction transaction = new Transaction()
         {
@@ -21,7 +21,8 @@ public static partial class TransactionExtensions
             Amount = 0,
             FromAccountNumber = transactionAddRequest.FromAccountNumber,
             ToAccountNumber = transactionAddRequest.ToAccountNumber,
-            DateTime = DateTime.Now
+            DateTime = DateTime.Now,
+            TransactionType = TransactionTypeOptions.Transfer
         };
 
         return transaction;

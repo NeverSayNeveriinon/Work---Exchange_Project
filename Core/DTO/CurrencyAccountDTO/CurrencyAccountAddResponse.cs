ecddaa25a050b449c987ece52d962fccb5448fac
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-27 16:11:53 +0330

    feat: Create New Required DTO's

---
+++ Core/DTO/CurrencyAccountDTO/CurrencyAccountAddResponse.cs
@@ -0,0 +1,29 @@
+﻿using Core.Domain.Entities;
+using Core.Enums;
+
+namespace Core.DTO.CurrencyAccountDTO;
+
+public class CurrencyAccountAddResponse : CurrencyAccountResponse
+{
+    public Guid TransactionId { get; set; }
+}
+
+
+public static partial class CurrencyAccountExtensions
+{
+    public static CurrencyAccountAddResponse ToCurrencyAccountResponse (this CurrencyAccount currencyAccount, Guid transactionId)
+    {
+        CurrencyAccountAddResponse response = new CurrencyAccountAddResponse()
+        {
+            Number = currencyAccount.Number,
+            OwnerID = currencyAccount.OwnerID,
+            FullName = currencyAccount.Owner?.PersonName,
+            Balance = currencyAccount.Balance,
+            TransactionId = transactionId,
+            CurrencyType = currencyAccount.Currency?.CurrencyType,
+            DateTimeOfOpen = currencyAccount.DateTimeOfOpen
+        };
+
+        return response;
+    }
+}
---
+++ Core/DTO/TransactionDTO/ConfirmTransactionRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO.TransactionDTO;
+
+public class ConfirmTransactionRequest
+{
+    [Required(ErrorMessage = "The 'TransactionId' Can't Be Blank!!!")]
+    public Guid TransactionId { get; set; }
+    
+    [AllowedValues("Confirmed","Cancelled", ErrorMessage = "The Value Must be 'Confirmed' or 'Cancelled'")]
+    [Required(ErrorMessage = "The 'TransactionStatus' Can't Be Blank!!!")]
+    public string TransactionStatus { get; set; }
+}

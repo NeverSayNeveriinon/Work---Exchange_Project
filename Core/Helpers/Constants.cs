a1f2b33531ea496c44f01a26d2f89ad0a04ff38d
Ali Hemmati <a.hemmati@shpaco.com>
2024-07-27 16:14:48 +0330

    feat: Create a static Class for 'Constants' Used in App

---
+++ Core/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Helpers;
+
+public static class Constants
+{
+      public const string AdminRole = "Admin";
+      public const string UserRole = "User";
+      public const string USDCurrency = "USD";
+      public const string DecimalMaxValue = "79228162514264337593543950335";
+}
